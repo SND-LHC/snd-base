8101cd61350b345c56d3ee6aff2a6fe2c1f42232
Oliver Lantwin <oliver.lantwin@cern.ch>
2017-10-17 12:09:35 +0200

    Copy genie data files to FairSoftInst

--- build_FairSoft.sh
+++ build_FairSoft.sh
@@ -9,4 +9,6 @@ cd $SHIPSOFT/FairSoft
 echo 1 | ./configure.sh
 
 # clean up
+rm $SHIPSOFT/FairSoftInst/share/genie
+mv $SHIPSOFT/FairSoft/generators/genie-2.10.0/data $SHIPSOFT/FairSoftInst/share/genie
 rm -rf $SHIPSOFT/FairSoft
