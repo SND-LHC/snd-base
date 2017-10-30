b16718558928308615dc9c3c1a606d95f16d990c
Oliver Lantwin <oliver.lantwin@cern.ch>
2017-10-30 17:06:45 +0100

    Fix typo

--- SHiPBuild.sh
+++ SHiPBuild.sh
@@ -4,7 +4,7 @@ export SHIPSOFT=SHiPBuild
 
 mkdir -p $SHIPSOFT && cd $SHIPSOFT
 
-git clone -b master https://github.com/ShipSoft/FairShip.git $SHIPSOFT/FairSoft
+git clone -b master https://github.com/ShipSoft/FairShip.git $SHIPSOFT/FairShip
 {
     cd $SHIPSOFT/FairShip
     git checkout v2-10
