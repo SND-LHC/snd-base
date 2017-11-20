ca076d84adb1af9a3ccc808aa162820b77292bb5
Oliver Lantwin <oliver.lantwin@cern.ch>
2017-11-20 14:51:50 +0100

    Unpin FairShip commit

--- SHiPBuild.sh
+++ SHiPBuild.sh
@@ -7,7 +7,7 @@ mkdir -p $SHIPSOFT && cd $SHIPSOFT
 git clone -b master https://github.com/ShipSoft/FairShip.git $SHIPSOFT/FairShip
 {
     cd $SHIPSOFT/FairShip
-    git checkout 9536ea46edcb225c5aba88c3ba1d94b284e6d346
+    # git checkout 9536ea46edcb225c5aba88c3ba1d94b284e6d34
 }
 cd $SHIPSOFT
 FairShip/aliBuild.sh
