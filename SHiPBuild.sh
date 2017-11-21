ad230b043da0363d572626cf8023a3df9265a0c0
Oliver Lantwin <oliver.lantwin@cern.ch>
2017-11-21 17:20:43 +0100

    Remove workaround now that shipdist was fixed

--- SHiPBuild.sh
+++ SHiPBuild.sh
@@ -4,8 +4,6 @@ export SHIPSOFT=/SHiPBuild
 
 pip install alibuild
 
-pip install matplotlib scipy certifi IPython==5.* ipywidgets metakernel
-
 mkdir -p $SHIPSOFT && cd $SHIPSOFT
 
 git clone https://github.com/ShipSoft/shipdist.git
